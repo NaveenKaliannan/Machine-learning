d7e8cda253d9e1e7e47144d11ea26b35c11da690
NaveenKaliannan <naveenkumar5892@gmail.com>
2022-01-17 20:20:26 +0100

    sci py added

---
+++ tutorials/3scipy.py
@@ -0,0 +1,7 @@
+##scipy-extension of numpy used for scientific and advanced mathematics algo and functions
+## it contains mathematical integration, linear algebra, statistics, image processing, language integration.
+## characteristics -  built in mathematical libraries, high level commands for data manipulation and visulatization, efficient and fast data processing, large libraries.
+
+## widely used packages - cluster algo (pattern), constants (mathematical and physical), fftpack (fast fourier transform), integrate (ode), spatial, interpolate (smooth splines), IO input and output, linalg (linear algebra), ndimage (N-dimensaional image processing), odr orthogonal distance regression , optimize, signal processing signal, sparse for sparse matrices, weave for c/c++ integration, stats for statistics, special for special function
+
+
