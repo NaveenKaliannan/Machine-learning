20f1be9915b394a644719938b76a5b8e1dd2f57e
NaveenKaliannan <naveenkumar5892@gmail.com>
2022-01-17 13:26:10 +0100

    numpy added

---
+++ tutorials/2numpy.py
@@ -0,0 +1,20 @@
+##numpy - package for mathematical computingin python
+## fast and efficient multidimension array
+## linear algebraic operation, fourier transformation
+## integrating c, c++
+
+import numpy as np
+
+distance = [10,15,17,26]
+time=[.30, .47, .55, 1.20]
+
+## error speed=distance/time without numpy
+
+distance = np.array(distance)
+time = np.array(time)
+speed = (distance/time)
+print(speed)
+
+##ndarray - used as the primary container to exchange data between algorithms
+## arrays can 1,2d, 3d or multi D
+## array([5,7,8]), array([],[]) array([],[],[])
