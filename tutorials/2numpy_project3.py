d6b579ec69f320bd85215aaee936f58bacc450a5
NaveenKaliannan <naveenkumar5892@gmail.com>
2022-01-19 19:17:33 +0100

    scipy course complete

--- tutorials/2numpy_project3.py
+++ tutorials/2numpy_project3.py
@@ -3,14 +3,16 @@ import numpy as np
 
 ## reading all the lines at once line = f.readlines(), list(f)
 ## reading line by line f.readline()
-
+data=[]
 data = np.loadtxt("/home/naveenk/temp/del/OOdistance/MgCl2", dtype='float')
 print(" Dimension = {}, shape = {} ".format(data.ndim,data.shape))
 nrow,ncolomn=data.shape
+
+print("without transpposing = {} ".format(np.dot(data[:, 0], data[:, 2]) ) )
+
 data=np.matrix.flatten(data)
 data = data.reshape(nrow,ncolomn)
 
 data=np.array(data)
 data=np.transpose(data)
-print("total OO distance = {}".format(np.dot(data[0], data[2])))
-print("total OO distance = {}".format(np.dot(data[:][0], data[:][2])))
+print("without transpposing = {} ".format(np.dot(data[0, :], data[2, :]) ) )
--- tutorials/3scipy.py
+++ tutorials/3scipy.py
@@ -5,10 +5,56 @@
 ## widely used packages - cluster algo (pattern), constants (mathematical and physical), fftpack (fast fourier transform), integrate (ode), spatial, interpolate (smooth splines), IO input and output, linalg (linear algebra), ndimage (N-dimensaional image processing), odr orthogonal distance regression , optimize, signal processing signal, sparse for sparse matrices, weave for c/c++ integration, stats for statistics, special for special function
 
 ## integrate function used for integration
+## quad means quad library from fortran
+##integrate.dblquad() .tplquad((), nquad()
 
+from numpy as np
 from scipy.integrate import quad
 
+## Optimisation package improves the performace mathematically by controlling the parameters
+## conjugate gradeint method
+## optimiser.minimize(). root() gives the root of a function
+## optimiser.curve_fit
+## in optimizer , method used should be defined method='bfgs'
 
-def integrateFunction(x):
-    return x
+## for cuve fitting, one needs to specify the upper limit and lower limit. 
+
+
+## Linear algebra 
+from scipy import linalg
+# linalg 
+## linalg.inv(matrix)
+## linalg.det(matrix)
+## linalg.solve(matrixA, vecx)
+## linalg.svd(matrixA)
+
+## eigenvalue, eigenvector = linalg.eig(matrix)
+## firsteigenvalue, secondeigenvalue =  eigenvalue
+## print(eigenvector[:][0], eigenvector[:,1])
+
+
+
+#statistics package
+##pdf(), cdf()
+## linregress()
+## describe(), normaltest()
+
+## cdf- cumulative probability total probability = 1
+## pdf - probability density function of random vairable
+## is the derivate of cdf with respect x
+
+from scipy.stats import norm
+
+## norm.rvs()
+## norm.cdf()
+## norm.pdf()
+
+## weave package - includes c/c++ library - speeds up to 1.5x to 30x  compared to algorithms writen python inline() blitz() for faster execution
+
+## IO packages. for file fomats, matlab files, idl files, matrix market files, arff files, netcdf files.
+
+##NumPy.loadtxt()/.savetxt()
+##NumPy.genfromtxt()/.recfromcsv()
+##NumPy.save()/NumPy.load()
+
 
