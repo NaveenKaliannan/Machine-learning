6dcf40e331b7bd89989a9074efe33aacb2d558db
NaveenKaliannan <naveenkumar5892@gmail.com>
2022-03-12 16:07:24 +0100

    Assignment optimum cluster added

--- tutorials/Assignments/Assignments1/task1.py
+++ tutorials/Assignments/Assignments1/task1.py
@@ -1,86 +1,94 @@
-from numpy import unique
+## Read paper - What to Do When K-Means Clustering Fails: A Simple yet Principled Alternative Algorithm
+##  https://datascience.stackexchange.com/questions/6508/k-means-incoherent-behaviour-choosing-k-with-elbow-method-bic-variance-explain
+## https://machinelearninginterview.com/topics/machine-learning/how-to-find-the-optimal-number-of-clusters-in-k-means-elbow-and-silhouette-methods/
+## https://medium.com/@masarudheena/4-best-ways-to-find-optimal-number-of-clusters-for-clustering-with-python-code-706199fa957c
+## Knee point detection algorithm -  https://practicaldatascience.co.uk/machine-learning/how-to-use-knee-point-detection-in-k-means-clustering
+## Silhoutee coefficient -  https://towardsdatascience.com/clustering-metrics-better-than-the-elbow-method-6926e1f723a6
+## Silhoutee coefficient - https://towardsdatascience.com/silhouette-coefficient-validating-clustering-techniques-e976bb81d10c
+# clustering dataset
+# determine k using elbow method
+from sklearn.cluster import KMeans
+from sklearn import metrics
+from scipy.spatial.distance import cdist
 import numpy as np
 import pandas as pd
+from numpy import unique
 from numpy import where
-from sklearn.mixture import GaussianMixture
-from sklearn.cluster import SpectralClustering
-from sklearn.cluster import OPTICS
-from sklearn.cluster import MeanShift
-from sklearn.cluster import MiniBatchKMeans
-from sklearn.cluster import KMeans
-from sklearn.cluster import DBSCAN
-from sklearn.cluster import Birch
-from sklearn.cluster import AgglomerativeClustering
-from sklearn.cluster import AffinityPropagation
-from matplotlib import pyplot 
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import silhouette_score
+from matplotlib import pyplot
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 
-pd.set_option('display.max_rows', None)
-marketdata = pd.read_csv("MarketData.csv")
-dataset=marketdata['Volume']
-newdataset=[ [i, j] for i, j in enumerate(marketdata['Volume']) ]
-dataset=np.array(newdataset)
+def get_keys_from_value(d, val):
+    return [k for k, v in d.items() if v == val]
 
 
-model = GaussianMixture(n_components=5)
-model.fit(dataset)
-y_gaussian = model.predict(dataset)
+## Reading the data
+df_marketdata = pd.read_csv("https://raw.githubusercontent.com/NaveenKaliannan/Machine-learning/main/tutorials/Assignments/Assignments1/MarketData.csv")
+newdataset=[ [i, j] for i, j in enumerate(df_marketdata['Volume']) ]
+newdataset=np.array(newdataset)
 
-plt.figure()
-y=y_gaussian
+## finding the optimum number of cluster using SILHOUETTE approach
+opt_cluster={}
+for i in range(2,5,1):
+  model = KMeans(n_clusters=i)
+  model.fit(newdataset)
+  y_kmean = model.predict(newdataset)  
+  opt_cluster[i] = opt_cluster.get(i, silhouette_score(newdataset, y_kmean))
+
+opt_cluster = get_keys_from_value(opt_cluster, max(opt_cluster.values()) )
+opt_cluster = opt_cluster[0]
+
+
+x1 = np.array(df_marketdata.index)
+x2 = np.array(df_marketdata['Volume'])
+X = np.array(list(zip(x1, x2))).reshape(len(x1), 2)
+colors = ['b', 'g', 'r']
+markers = ['o', 'v', 's']
+# k means determine k
+distortions = []
+K = range(1,8)
+for k in K:
+  kmeanModel = KMeans(n_clusters=k).fit(X)
+  kmeanModel.fit(X)
+  distortions.append(sum(np.min(cdist(X, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])
+
+
+## Performing the K-Means cluster with an optimum cluster
+model = KMeans(n_clusters=opt_cluster)
+model.fit(newdataset)
+y_kmean = model.predict(newdataset)  
+
+
+## plotting the simulation result
+fig, axs = plt.subplots(2, 1, figsize=(6,9), dpi=100)
+figure(figsize=(6,9), dpi=100 )
+
+axs[0].plot(K, distortions, 'bx-')
+axs[0].scatter(opt_cluster, distortions[opt_cluster-1] , s=80, color = 'red', label="Optimum cluster = " + str(opt_cluster) )
+axs[0].set_xlabel('Cluster number ')
+axs[0].set_ylabel('Distortion')
+axs[0].set_title('Elbow Method')
+
+y=y_kmean
 clusters = unique(y)
 for cluster in clusters:
   index = where(y == cluster)
-  plt.scatter(dataset[index, 0],dataset[index, 1],s=0.2)
-plt.xlabel('t  (days)')    
-plt.ylabel('Volume (arb. unit)') 
-plt.show() 
+  axs[1].scatter(newdataset[index, 0],newdataset[index, 1],s=0.2)
+axs[1].set_xlabel('Time (days)')    
+axs[1].set_ylabel('Volume (arb. unit)') 
+axs[1].set_title('K means cluster')
 
-## Gaussian Mixture
-model = GaussianMixture(n_components=2)
-model.fit(dataset)
-y_gaussian = model.predict(dataset)
-        
-## Spectral clustering
-model = SpectralClustering(n_clusters=2)
-y_spectral = model.fit_predict(dataset)
+axs[1].set_xticks([i for i in range(0, 5232, 261)]) 
+a=axs[1].get_xticks().tolist()
+a=np.arange(2000, 2021, 1)
+axs[1].set_xticklabels(a, rotation = 90.0)
+##axs[1].set_xticks(np.arange(1, 5232, 261), np.arange(2000, 2021, 1), rotation = 45) 
+##plt.xticks( rotation='vertical')
 
-## Optics clustering
-model = OPTICS(eps=0.8, min_samples=10)
-y_optics = model.fit_predict(dataset)
+fig.savefig("optimumcluster.png", format="png")
+plt.show()
+plt.close()
 
-##Mean shift
-model = MeanShift()
-y_meanshift = model.fit_predict(dataset)
-
-##Minibatch k means
-model = MiniBatchKMeans(n_clusters=2)
-model.fit(dataset)
-y_minikmean = model.predict(dataset)
-
-##Kmeans
-model = KMeans(n_clusters=5)
-model.fit(dataset)
-y_kmean = model.predict(dataset)
-
-##Db scan
-model = DBSCAN(eps=0.30, min_samples=9)
-y_dbscan = model.fit_predict(dataset)
-
-##Birch
-model = Birch(threshold=0.01, n_clusters=2)
-model.fit(dataset)
-y_birch = model.predict(dataset)
-
-##Agglomerativeclustering
-model = AgglomerativeClustering(n_clusters=2)
-y_agglo = model.fit_predict(dataset)
-
-##Affinity
-model = AffinityPropagation(damping=0.9)
-model.fit(dataset)
-y_affinity = model.predict(dataset)
-
- 
 
--- tutorials/Assignments/Assignments1/task3.py
+++ tutorials/Assignments/Assignments1/task3.py
@@ -1,7 +1,6 @@
-
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot 
+from matplotlib import pyplot
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 
@@ -10,20 +9,8 @@ def zeroadder(x):
     if(x < 10):
         x = str('0')+ str(x)
     return str(x)
-    
-def mothly_grade(rating):
-    if rating > 0.4 :
-        return 'Profit L'
-    if rating > 0.0:
-        return 'Profit S'
-    if rating == 0.0:
-        return 'Null'
-    if rating > -0.2:
-        return 'Loss S'
-    else:
-        return 'Loss L'    
-
-df = pd.read_csv("MarketData.csv")
+   
+df = pd.read_csv("https://raw.githubusercontent.com/NaveenKaliannan/Machine-learning/main/tutorials/Assignments/Assignments1/MarketData.csv")
 years= [zeroadder(i) for i in range(2000, 2021, 1)]
 months= [zeroadder(i) for i in range(1, 13, 1)]
 yearmonths={i+'-'+j:[] for i in years for j in months}
@@ -55,21 +42,28 @@ mrdate_max= ''
 mrdate_min= ''
 for k,v in mr.items():
     if(mr_max==v):
-        print("max monthly returns = ", k, v)
+        print("Max (Montly returns) = {}, Month {} ".format(k, v))
         mrdate_max= k
     elif(mr_min==v):
-        print("min montly returns = ", k, v)
+        print("Min (Montly returns) = {}, Month {} ".format(k, v))
         mrdate_min= k
 
 plt.figure()
-plt.scatter(list(mr).index(mrdate_min), mr_min, s=25,label="Minimum montly returns")
-plt.scatter(list(mr).index(mrdate_max), mr_max, s=25,label="Maximum montly returns")
+plt.scatter(list(mr).index(mrdate_min), mr_min, s=40,label="Min (montly returns) - " + mrdate_min )
+plt.scatter(list(mr).index(mrdate_max), mr_max, s=40,label="Max (montly returns) - " + mrdate_max)
+ 
+gf_mon =df_mr.groupby('Month').sum().sort_values(by='Monthly-Return',ascending=False)
+maxmon=list(gf_mon.max())
+minmon=list(gf_mon.min())
+maxmon=maxmon[0]
+minmon=minmon[0]
+print("Best Month to invest is {}, Worst Month to invest is {}.".format(gf_mon.index[0], gf_mon.index[-1] ) )
+plt.scatter(list(df_mr.loc[df_mr["Month"] == gf_mon.index[0]].index), list(df_mr["Monthly-Return"].loc[df_mr["Month"] == gf_mon.index[0]]), s = 35, marker="s", label="Best month to invest - " + gf_mon.index[0] )
+plt.scatter(list(df_mr.loc[df_mr["Month"] == gf_mon.index[-1]].index), list(df_mr["Monthly-Return"].loc[df_mr["Month"] == gf_mon.index[-1]]), s = 35, marker="d", label="Worst Month to invest - " + gf_mon.index[-1] )
 plt.scatter(df_mr.index,df_mr['Monthly-Return'],s=4,label="All data")
+plt.xticks(np.arange(1, 250, 12), np.arange(2000, 2021, 1), rotation='vertical')
 plt.xlabel('Time  (Months)')    
-plt.ylabel('Monthly Returns (arb. unit)') 
-plt.legend() 
-plt.show()   
-
-
-print(df_mr.groupby('Month').sum().applymap(mothly_grade))
+plt.ylabel('Monthly Returns (arb. unit)')
+plt.legend(loc='best')
+plt.show()
 
